2b9cc002cede01bdfd86f18a61d692f43a4f0752
roysteven <roychenvca@gmail.com>
2023-10-03 17:01:45 +0000

    Daily Update

--- designpaterns/base/log.js
+++ designpaterns/base/log.js
@@ -2,8 +2,9 @@ let log = (function(ctx){
     const DEBUG_TITLE = '[Debug]';
     const  INFO_TITLE = '[INFO]';
     const TRACE_TITLE = '[TRACE]';
-
+    
     return {
+        whoami: function whoami(s){console.log("Start module named:", this.name, this);},
         debug: (s)=>{console.log(DEBUG_TITLE, s);},
         info:  (s)=>{console.log(INFO_TITLE, s);},
         trace: (s)=>{console.log(TRACE_TITLE, s);}

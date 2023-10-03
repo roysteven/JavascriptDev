384e8127aad82d715fa5c6f81cfe03f55a426b20
roysteven <roychenvca@gmail.com>
2023-10-03 17:02:20 +0000

    Works

--- designpaterns/dp.factory.js
+++ designpaterns/dp.factory.js
@@ -1,13 +1,9 @@
 // Factory design patern
-function whoami() {
-    console.log("Start the name:", this.name);
-};
 
 function FactoryDesignPatern() {
     this.name = 'Factory design patern';
 }
 
 module.exports = {
-    whoami: whoami,
     FactoryDesignPatern: FactoryDesignPatern
 }
--- main.js
+++ main.js
@@ -3,12 +3,10 @@ var fp = require('./designpaterns/dp.factory');
 var m_base = require('./designpaterns/base/base');
 let Log = m_base.Log;
 let Test = m_base.Test;
+let whoami = Log.whoami;
 
 console.log("Testing");
 let f = fp.FactoryDesignPatern;
-fp.whoami.call(f, "");
+whoami.call(f, "");
 
 Test('Testing Module works');
-Log.debug('Debug Module works');
-Log.info('Info Module works');
-Log.trace('Trace Module works');
